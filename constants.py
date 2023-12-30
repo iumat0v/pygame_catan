2a7ee2df4f5d070636d18379b87783749f0c3ab3
Алексей <iumat0v@yandex.ru>
2023-12-30 16:20:44 +0300

    -

---
+++ constants.py
@@ -0,0 +1,27 @@
+import pygame
+from functions import load_image
+
+CELL_SIZE = 45
+tile_images = {
+    "Глинянный карьер": load_image("Глинянный карьер.png", (CELL_SIZE * 2, CELL_SIZE * 2), 30, - 1),
+    "Гора": load_image("Гора.png", (CELL_SIZE * 2, CELL_SIZE * 2), 30, - 1),
+    "Лес": load_image("Лес.png", (CELL_SIZE * 2, CELL_SIZE * 2), 30, - 1),
+    "Пашня": load_image("Пашня.png", (CELL_SIZE * 2, CELL_SIZE * 2), 30, - 1),
+    "Пустыня": load_image("Пустыня.png", (CELL_SIZE * 2, CELL_SIZE * 2), 30, - 1),
+    "Луг": load_image("Луг.png", (CELL_SIZE * 2, CELL_SIZE * 2), 30, - 1)
+}
+tile_group = pygame.sprite.Group()
+VER = {
+    "2": 1,
+    "3": 2,
+    "4": 3,
+    "5": 4,
+    "6": 5,
+    "8": 5,
+    "9": 4,
+    "10": 3,
+    "11": 2,
+    "12": 1
+}
+size = (701, 701)
+FPS = 10
---
+++ functions.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import pygame_gui
+import pygame
+from constants import *
+
+pygame.init()
+size = (701, 701)
+screen = pygame.display.set_mode(size)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def exit(manager):
+    dialog = pygame_gui.windows.UIConfirmationDialog(
+        rect=pygame.Rect(250, 250, 300, 300),
+        manager=manager,
+        window_title="Подтверждение выхода",
+        action_long_desc="Вы действительно хотите выйти?",
+        action_short_name="Ok",
+        blocking=True
+    )
+
+
+def show_text(screen, tex, pos):
+    color = (0, 0, 0)
+    if tex in "68":
+        color = (255, 0, 0)
+    font = pygame.font.Font(None, 30)
+    text = font.render(tex, 1, color)
+    screen.blit(text, pos)
+
+
+def load_image(name, size, angle=0, colorkey=None, direct="images"):
+    fullname = os.path.join(direct, name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    image = pygame.transform.scale(image, size)
+    image = pygame.transform.rotate(image, angle)
+    return image
+
+
+def start_screen(screen):
+    manager = pygame_gui.UIManager(size)
+    fon = load_image("fon.jpg", size)
+    screen.blit(fon, (0, 0))
+    switch_start = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((10, size[1] - 110), (150, 50)),
+        text="Старт",
+        manager=manager
+    )
+    switch_exit = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((10, size[1] - 60), (150, 50)),
+        text="Выход из игры",
+        manager=manager
+    )
+    clock = pygame.time.Clock()
+    while True:
+        time_delta = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit(manager)
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_CONFIRMATION_DIALOG_CONFIRMED:
+                    terminate()
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == switch_start:
+                        return
+                    if event.ui_element == switch_exit:
+                        exit(manager)
+
+            manager.process_events(event)
+        screen.blit(fon, (0, 0))
+        manager.update(time_delta)
+        manager.draw_ui(screen)
+        pygame.display.flip()
+        #clock.tick(FPS)
+    pass
